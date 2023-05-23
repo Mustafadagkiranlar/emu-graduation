ea0365278986b7db139e30d6366e51dc66e6157c
Mustafadagkiranlar <dagkiranlar.m@gmail.com>
2023-05-23 17:25:32 +0300

    init

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Jaydon Frankie',
+  displayName: 'Mustafa Dağkıranlar',
   email: 'demo@minimals.cc',
   photoURL: '/assets/images/avatars/avatar_default.jpg',
 };
